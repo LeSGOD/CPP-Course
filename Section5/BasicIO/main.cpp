1007f59b277c5a0d58343b0561ef92743b921392
Artur Leszczynski <artles00@wp.pl>
2020-07-22 19:05:10 +0200

    section5

---
+++ Section5/BasicIO/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    //cout << "Hello\nOut\nThere\n";
+    
+    int num1;
+    int num2;
+    double num3;
+    
+//    cout << "Enter an integer ";
+//    cin >> num1;
+//    cout << "You entered: " << num1 << endl;
+
+//    cout << "Enter first integer: ";
+//    cin >> num1;
+//    
+//    cout << "Enter second integer: ";
+//    cin >> num2;
+//    
+//    cout << "You entered " << num1 << " and " << num2 << endl;
+
+    cout << "Enter 2 integers separeted with a space: ";
+    cin >> num1 >> num2;
+    cout << "You entered " << num1 << " and " << num2 << endl;
+    return 0;
+}
