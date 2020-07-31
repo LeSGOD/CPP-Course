750af48f8b37b8f28c75e1381ed67220c2cc6643
Artur Leszczynski <artles00@wp.pl>
2020-07-31 07:32:18 +0200

    CPP

---
+++ edabit/ReplacingLetterswithHashes/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <iterator>
+using namespace std;
+
+//Write a function that replaces all letters within a specified range with the hash symbol #.
+//
+//Examples
+//replace("abcdef", "c-e") ➞ "ab###f"
+//
+//replace("rattle", "r-z") ➞ "#a##le"
+//
+//replace("microscopic", "i-i") ➞ "m#croscop#c"
+//
+//replace("", "a-z") ➞ ""
+
+std::string replace(std::string str, std::string r) 
+{
+	
+}
+
+int main() {
+    std::cout <<"Hello World!"<<std::endl;
+    return 0;
+}
