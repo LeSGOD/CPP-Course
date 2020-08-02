261b42ff2277efaca8c2116019c0935a0f542538
Artur Leszczynski <artles00@wp.pl>
2020-08-02 23:22:25 +0200

    CPgit P

---
+++ edabit/TriangularNumberSequence/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+//This Triangular Number Sequence is generated from a pattern of dots that form a triangle. The first 5 numbers of the sequence, or dots, are:
+//
+//1, 3, 6, 10, 15
+//This means that the first triangle has just one dot, the second one has three dots, the third one has 6 dots and so on.
+//
+//Write a function that gives the number of dots with its corresponding triangle number of the sequence.
+//
+//Examples
+//triangle(1) ➞ 1
+//
+//triangle(6) ➞ 21
+//
+//triangle(215) ➞ 23220
+int triangle(int n) {
+    int result {};
+	for(int i=n; i > 0; i--){
+        result += i;
+    }
+    cout << result << endl;
+}
+
+int main() { 
+    triangle(6);
+    return 0;
+}
